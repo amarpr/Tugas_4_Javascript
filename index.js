871b748a757220f2ed8c23c46a25c93d1d650f42
Muhamar <amararrasyid@gmail.com>
2020-05-03 12:53:20 +0700

    Tugas 4 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,26 @@
+var tertinggi = 100;
+var tertinggiKedua = 99;
+var terendah = 98;
+
+
+if (tertinggi === 100) {
+    console.log("Siswa yang memiliki tinggi badan tertinggi adalah " + tertinggi + " cm");
+} else {
+    console.log("Bukan tertinggi")
+}
+
+
+
+if (tertinggiKedua === 99) {
+    console.log("Siswa yang memiliki tinggi badan tertinggi kedua adalah " + tertinggiKedua + " cm");
+} else {
+    console.log("Bukan tertinggi kedua");
+}
+
+
+
+if (terendah === 98) {
+    console.log("Siswa yang memiliki tinggi badan terendah adalah " + terendah + " cm")
+} else {
+    console.log("Bukan terendah");
+}
